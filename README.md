e1806acfbff407ac4c4a4a6ad0ccf9cc0ecacc39
Steven Turturo <steven@turturo.dev>
2025-07-04 18:40:29 -0400

    feat: initial commit with generated client from oapi spec

---
+++ README.md
@@ -0,0 +1,2 @@
+# logto-go
+A simple Go SDK to make using the Logto management API a little easier.
---
+++ logto.go
@@ -0,0 +1,3 @@
+package logto
+
+//go:generate go tool oapi-codegen -config oapi.config.yml ./logto-openapi-source.json
