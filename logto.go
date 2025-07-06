cb40a80cae72ecd548ca9918e9a598ea4461c39c
Steven Turturo <steven@turturo.dev>
2025-07-06 17:06:23 -0400

    chore: start fleshing out how to setup and use the logto client

--- logto.go
+++ logto.go
@@ -1,3 +1,17 @@
 package logto
 
 //go:generate go tool oapi-codegen -config oapi.config.yml ./logto-openapi-source.json
+
+type LogtoClient struct {
+	BaseURL string
+}
+
+type LogtoClientConfig struct {
+	BaseURL string
+}
+
+func NewLogtoClient(config LogtoClientConfig) *LogtoClient {
+	return &LogtoClient{
+		BaseURL: config.BaseURL,
+	}
+}
